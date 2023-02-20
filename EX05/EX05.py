4b8b1f45515cd5503713564c8f5cdc90a53953d6
heohak <heohak@ttu.ee>
2023-02-20 14:52:29 +0200

    ex05

---
+++ EX05/EX05.py
@@ -0,0 +1,49 @@
+"""OT05 - Noise."""
+import PiBot
+import statistics
+
+class Robot:
+    """Robot class."""
+
+    def __init__(self):
+        """Initialize object."""
+        self.robot = PiBot.PiBot()
+        self.shutdown = False
+        self.values = []
+
+    def set_robot(self, robot: PiBot.PiBot()) -> None:
+        """Set the PiBot reference."""
+        self.robot = robot
+
+    def sense(self):
+        """Read values."""
+        front_laser = self.robot.get_front_middle_laser()
+        self.values.append(front_laser)
+
+    def get_front_middle_laser(self) -> float:
+        """
+        Return the filtered value.
+
+        Returns:
+          None if filter is empty, filtered value otherwise.
+        """
+        return statistics.median(self.values)
+        # Your code here...
+
+    def spin(self):
+        """The spin loop."""
+        while not self.shutdown:
+            print(f'Value is {self.get_front_middle_laser()}')
+            self.robot.sleep(0.05)
+            if self.robot.get_time() > 20:
+                self.shutdown = True
+
+
+def main():
+    """The main entry point."""
+    robot = Robot()
+    robot.spin()
+
+
+if __name__ == "__main__":
+    main()
