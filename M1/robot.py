1dc4a5910c921fcbe11d7fe27ca11d118190e08a
heohak <heohak@ttu.ee>
2023-04-27 13:53:03 +0300

    uus

--- M1/robot.py
+++ M1/robot.py
@@ -34,9 +34,7 @@ class Robot:
 
         self.is_first = True
 
-        self.can_turn_left = True
-        self.can_turn_right = False
-
+        self.turning = None
         self.state = "forward"
 
         self.left_rear_side = 0
@@ -50,45 +48,9 @@ class Robot:
         self.medians_list_r = []
         self.data_r = []
 
-        self.medians_list_front_r = []
-        self.medians_list_front_l = []
-
-        self.data_front_r = []
-        self.data_front_l = []
-
-        self.count = 0
-
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """Set Robot reference."""
         self.robot = robot
-
-    def get_right_front(self):
-        """
-        Return the filtered value.
-
-        Returns:
-          None if filter is empty, filtered value otherwise.
-        """
-        if self.right_rear_str:
-            if len(self.data_front_r) < 7:
-                self.data_front_r.append(self.right_rear_str)
-            else:
-                self.data_front_r.pop(0)
-                self.data_front_r.append(self.right_rear_str)
-
-    def get_left_front(self):
-        """
-        Return the filtered value.
-
-        Returns:
-          None if filter is empty, filtered value otherwise.
-        """
-        if self.left_rear_str:
-            if len(self.data_front_l) < 7:
-                self.data_front_l.append(self.left_rear_str)
-            else:
-                self.data_front_l.pop(0)
-                self.data_front_l.append(self.left_rear_str)
 
     def get_right_laser(self):
         """
@@ -128,10 +90,6 @@ class Robot:
 
         self.get_right_laser()
         self.get_left_laser()
-
-        self.get_right_front()
-        self.get_left_front()
-
         if self.is_first:
             self.right_side_previous = self.right_rear_side
             self.left_side_previous = self.left_rear_side
@@ -142,143 +100,73 @@ class Robot:
     def plan(self):
         """Plan robot."""
         if self.state == "forward":
-            self.forward()
-        elif self.state == "drive_towards_wall_left":
-            self.drive_towards_wall_left()
-        elif self.state == "drive_towards_wall_right":
-            self.drive_towards_wall_right()
+            self.left_wheel_speed = -10
+            self.right_wheel_speed = -10
+            if abs(self.left_rear_side - self.left_rear_side_begin) > 30 and self.left_rear_side > self.right_rear_side:
+                print("num1")
+                self.left_wheel_speed = -10
+                self.right_wheel_speed = -9
+            elif abs(self.right_rear_side - self.right_rear_side_begin) > 30 and self.right_rear_side > self.left_rear_side:
+                print("num2")
+                self.left_wheel_speed = -9
+                self.right_wheel_speed = -10
+            else:
+                if self.last_turn == "left":
+                    if self.rotation > 3:
+                        self.left_wheel_speed = 8
+                        self.right_wheel_speed = -8
+                    elif self.rotation < 0:
+                        self.left_wheel_speed = -8
+                        self.right_wheel_speed = 8
+                elif self.last_turn == "right":
+                    if self.rotation > -177:
+                        self.left_wheel_speed = 8
+                        self.right_wheel_speed = -8
+                    elif self.rotation < -180:
+                        self.left_wheel_speed = -8
+                        self.right_wheel_speed = 8
+        self.plan2()
+
+    def plan2(self):
+        """Create plan 2."""
+        if self.state == "forward":
+            if statistics.median(self.data_r) < 300 and statistics.median(self.data_l) < 400:
+                self.left_wheel_speed = 0
+                self.right_wheel_speed = 0
+                self.shutdown = True
+            elif statistics.median(self.data_r) < 300:
+                self.left_wheel_speed = 0
+                self.right_wheel_speed = 0
+                self.left_rear_str_previous = self.left_rear_str
+                self.right_rear_str_previous = self.right_rear_str
+                self.state = "turn_left"
+            elif statistics.median(self.data_l) < 400:
+                self.left_wheel_speed = 0
+                self.right_wheel_speed = 0
+                self.left_rear_str_previous = self.left_rear_str
+                self.right_rear_str_previous = self.right_rear_str
+                self.state = "turn_right"
+            else:
+                self.right_side_previous = self.right_rear_side
+                self.left_side_previous = self.left_rear_side
         elif self.state == "turn_left":
-            self.turn_left()
+            self.left_wheel_speed = -99
+            self.right_wheel_speed = -6
+            if self.rotation > 5:
+                self.left_wheel_speed = 0
+                self.right_wheel_speed = 0
+                self.data_l = []
+                self.last_turn = "left"
+                self.state = "forward"
         elif self.state == "turn_right":
-            self.turn_right()
-        elif self.state == "forward_until_between_walls":
-            self.forward_until_between_walls()
-
-    def adjust_left(self):
-        """Adjust after left turn."""
-        if self.rotation > 3:
-            self.left_wheel_speed = 8
-            self.right_wheel_speed = -8
-        elif self.rotation < 0:
-            self.left_wheel_speed = -8
-            self.right_wheel_speed = 8
-
-    def adjust_right(self):
-        """Adjust after right turn."""
-
-    def forward(self):
-        """State forward."""
-        self.left_wheel_speed = -10
-        self.right_wheel_speed = -10
-        if abs(self.left_rear_side - self.left_rear_side_begin) > 30 and self.left_rear_side > self.right_rear_side:
-            print("num1")
-            self.left_wheel_speed = -10
-            self.right_wheel_speed = -9
-        elif abs(self.right_rear_side - self.right_rear_side_begin) > 30 and self.right_rear_side > self.left_rear_side:
-            print("num2")
-            self.left_wheel_speed = -9
-            self.right_wheel_speed = -10
-        else:
-            if self.last_turn == "left":
-                self.adjust_left()
-            elif self.last_turn == "right":
-                self.adjust_right()
-        if statistics.median(self.data_r) < 400 and statistics.median(self.data_l) < 400:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.shutdown = True
-        elif statistics.median(self.data_r) < 300 and self.can_turn_left:  # ORIGINAALIS 300
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.state = "drive_towards_wall_left"
-        elif statistics.median(self.data_l) < 400 and self.can_turn_right:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.state = "drive_towards_wall_right"
-        else:
-            self.right_side_previous = self.right_rear_side
-            self.left_side_previous = self.left_rear_side
-
-    def drive_towards_wall_left(self):
-        """Drive towards back wall."""
-        if statistics.median(self.data_front_r) > 500 and statistics.median(
-                self.data_front_l) > 500:  # OTSI NEED NUMBRID ÜLES ÕIGEL ROBOTIL
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.rotation_previous = self.rotation
-            self.state = "turn_left"
-        else:
-            self.left_wheel_speed = -10
-            self.right_wheel_speed = -10
-
-    def turn_left(self):
-        """Turn left."""
-        if abs(self.rotation - self.rotation_previous) >= 88 and self.count == 0:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.data_front_l = []
-            self.data_front_r = []
-            self.data_l = []
-            self.state = "drive_towards_wall_left"
-            self.count = 1
-        elif abs(self.rotation - self.rotation_previous) >= 88 and self.count == 1:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.data_l = []
-            self.data_front_l = []
-            self.data_front_r = []
-            self.count = 0
-            self.state = "forward_until_between_walls"
-            self.last_turn = "left"
-            self.can_turn_left = False
-            self.can_turn_right = True
-        else:
-            self.left_wheel_speed = -8
-            self.right_wheel_speed = 8
-
-    def drive_towards_wall_right(self):
-        """Drive towards back wall."""
-        if statistics.median(self.data_front_r) > 500 and statistics.median(
-                self.data_front_l) > 500:  # OTSI NEED NUMBRID ÜLES ÕIGEL ROBOTIL
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.rotation_previous = self.rotation
-            self.state = "turn_right"
-        else:
-            self.left_wheel_speed = -10
-            self.right_wheel_speed = -10
-
-    def turn_right(self):
-        """Turn right."""
-        if abs(self.rotation - self.rotation_previous) >= 88 and self.count == 0:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.data_front_l = []
-            self.data_front_r = []
-            self.data_l = []
-            self.state = "drive_towards_wall_right"
-            self.count = 1
-        elif abs(self.rotation - self.rotation_previous) >= 88 and self.count == 1:
-            self.left_wheel_speed = 0
-            self.right_wheel_speed = 0
-            self.data_l = []
-            self.data_front_l = []
-            self.data_front_r = []
-            self.count = 0
-            self.state = "forward_until_between_walls"
-            self.last_turn = "right"
-            self.can_turn_right = False
-            self.can_turn_left = True
-        else:
-            self.left_wheel_speed = 8
-            self.right_wheel_speed = -8
-
-    def forward_until_between_walls(self):
-        """Drive forward until robot between walls."""
-        if statistics.median(self.data_r) > 500 and statistics.median(self.data_l) > 500:  # NEED NUMBRID KA
-            self.state = "forward"
-        self.left_wheel_speed = -10
-        self.right_wheel_speed = -10
+            self.left_wheel_speed = -7
+            self.right_wheel_speed = -99
+            if self.rotation < -185:
+                self.left_wheel_speed = 0
+                self.right_wheel_speed = 0
+                self.data_r = []
+                self.last_turn = "right"
+                self.state = "forward"
 
     def act(self):
         """Act robot."""
@@ -288,13 +176,10 @@ class Robot:
     def spin(self):
         """Create the main loop."""
         while not self.shutdown:
-            print(f"right_rear: {abs(self.right_rear_side)}")
-            print(f"left rear: {abs(self.left_rear_side)}")
-            print(f"tagumine otse: {self.right_rear_str}")  # TAGUMINE OTSE
-            print(f"tagumine otse: {self.left_rear_str}")  # TAGUMINE OTSE
-           # print(self.rotation)
-           # print(abs(self.rotation % 180))
-           # print(self.state)
+            print(abs(self.right_rear_side - self.right_rear_side_begin))
+            print(abs(self.left_rear_side - self.left_rear_side_begin))
+            print(self.rotation)
+            print(self.state)
             self.sense()
             self.plan()
             self.act()
