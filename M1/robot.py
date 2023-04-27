a109d8aef9d8fefcad265047056772d18f90a9ae
heohak <heohak@ttu.ee>
2023-04-27 13:13:48 +0300

    uus

--- M1/robot.py
+++ M1/robot.py
@@ -288,6 +288,7 @@ class Robot:
     def spin(self):
         """Create the main loop."""
         while not self.shutdown:
+            print(f"data faking front r {statistics.median(self.data_front_r)}")
             print(f"right_rear: {abs(self.right_rear_side)}")
             print(f"left rear: {abs(self.left_rear_side)}")
             print(f"tagumine otse: {self.right_rear_str}")  # TAGUMINE OTSE
